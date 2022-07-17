ff42db33d227f89d23e50acbeb79ec9fef0ad8fb
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-17 20:13:20 +0800

    Correct typos `development`

--- activate_code_server/__init__.py
+++ activate_code_server/__init__.py
@@ -2,14 +2,14 @@
 Activate Code Server
 ~~~~~~~~~~~~~~~~~~~~~~~~~
 Activate code system server
-:copyright: (c) 2022-present checore developer owner
+:copyright: (c) 2022-present checore development owner
 :license: MIT, see License for more details.
 """
 
 __title__ = 'activate_code_server'
-__author__ = 'checore developerment owner'
+__author__ = 'checore development owner'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2022-present checore developerment owner'
+__copyright__ = 'Copyright 2022-present checore development owner'
 __version__ = '0.1.0'
 
 from .server import *
