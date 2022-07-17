f6ce6dda6dbebe9deab6c18d8534f2d60c424c28
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-17 15:17:20 +0800

    Add `datebase_name` option

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ server = activate_code_server.app_server(
     host='localhost',
     port=7000,
     database_engine='sqlite',
-    database_filename='database'
+    database_filename='database',
+    database_name='activate_code_server'
 )
 
 if __name__ == "__main__":
