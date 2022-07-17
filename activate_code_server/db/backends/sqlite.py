67f37919d7c4e1ec8554a47dd29775dcab9c6a0b
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-17 22:51:09 +0800

    Setup `insert` function

--- activate_code_server/db/backends/sqlite.py
+++ activate_code_server/db/backends/sqlite.py
@@ -1,14 +1,20 @@
 import sqlite3
 
 sql_create_table = '''\
-CREATE TABLE IF NOT EXISTS "%s"(
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    TOKEN TEXT NOT NULL,
-    USAGE_COUNT INTEGER,
-    LIMIT_COUNT INTEGER NOT NULL,
-    CREATE_DATE INTEGER NOT NULL,
-    EXPIRATION_DATE INTEGER NOT NULL
+    CREATE TABLE IF NOT EXISTS "%s" (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        TOKEN TEXT NOT NULL,
+        USAGE_COUNT INTEGER NOT NULL,
+        LIMIT_COUNT INTEGER NOT NULL,
+        CREATE_DATE INTEGER NOT NULL,
+        EXPIRATION_DATE INTEGER NOT NULL
     )
+'''
+
+sql_insert = '''\
+    INSERT INTO "%s" 
+        (TOKEN, USAGE_COUNT, LIMIT_COUNT, CREATE_DATE, EXPIRATION_DATE)
+        VALUES (?, ?, ?, ?, ?)
 '''
 
 class sqlite():
@@ -26,4 +32,14 @@ class sqlite():
         cursor = self.db.cursor()
         cursor.execute(sql_create_table % self.database_name)
         cursor.close()
+    
+    def insert(self, *args, **kwargs):
+        token = kwargs.get('token')
+        limit = kwargs.get('limit', 1)
+        create_date = kwargs.get('create_date')
+        expiration_date = kwargs.get('expiration_date')
         
+        cursor = self.db.cursor()
+        cursor.execute(sql_insert % self.database_name, (token, 0, limit, create_date, expiration_date))
+        self.db.commit()
+        cursor.close()
