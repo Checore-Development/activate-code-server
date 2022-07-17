1b8395e458d3bd6dea2d3a7fe90ecb27cf5ed198
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-17 13:47:23 +0800

    Implement `MIT`

--- activate_code_server/__init__.py
+++ activate_code_server/__init__.py
@@ -1,2 +1,16 @@
+"""
+Activate Code Server
+~~~~~~~~~~~~~~~~~~~~~~~~~
+Activate code system server
+:copyright: (c) 2022-present checore developer owner
+:license: MIT, see License for more details.
+"""
+
+__title__ = 'activate_code_server'
+__author__ = 'checore developerment owner'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present checore developerment owner'
+__version__ = '0.1.0'
+
 from .server import *
 from .app import *
