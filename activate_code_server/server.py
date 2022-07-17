28c4d0e33c3f3105cfe5cb1d382b40fb947c5e62
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-17 13:34:02 +0800

    Add `notes`

--- activate_code_server/server.py
+++ activate_code_server/server.py
@@ -5,20 +5,20 @@ from .db import database
 
 class app_server:
     def __init__(self, *args, **kwargs):
-        self.host = kwargs.get('host', 'localhost')
-        self.port = kwargs.get('port', 8000)
-        self.database_engine = kwargs.get('database_engine', 'sqlite')
-        self.database_filename = kwargs.get('database_filename', 'database')
-        self.database_host = kwargs.get('database_host', '127.0.0.1')
-        self.database_port = kwargs.get('database_port')
-        self.database_name = kwargs.get('database_name', 'activate_code_server')
-        self.database_user = kwargs.get('database_user', 'root')
-        self.database_password = kwargs.get('database_password', '12345678')
-        self.app = None
-        self.server = None
-        self.database = None
-        self.setup_app()
-        self.setup_database()
+        self.host = kwargs.get('host', 'localhost') # hostname or IP address
+        self.port = kwargs.get('port', 8000) # hostport or port number
+        self.database_engine = kwargs.get('database_engine', 'sqlite') # database engine (sqlite, mysql, postgresql, mongodb)
+        self.database_filename = kwargs.get('database_filename', 'database') # database filename 
+        self.database_host = kwargs.get('database_host', '127.0.0.1') # database host (localhost, 127.0.0.1)
+        self.database_port = kwargs.get('database_port') # database port (3306, 5432, 27017)
+        self.database_name = kwargs.get('database_name', 'activate_code_server') # database name
+        self.database_user = kwargs.get('database_user', 'root') # database user (root)
+        self.database_password = kwargs.get('database_password', '12345678') # database password
+        self.app = None # flask app
+        self.server = None # flask server
+        self.database = None # database
+        self.setup_app() # setup flask app
+        self.setup_database() # setup database
         
     def setup_app(self):
         self.app = Flask(__name__)
