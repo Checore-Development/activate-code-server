67211a5558802cb36b1d4a3ecf6bbfd50f2b51ce
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-21 03:35:11 +0800

    Add `get_columns_name` `get_column_info`  `get_columns`

--- activate_code_server/db/backends/sqlite3/schema.py
+++ activate_code_server/db/backends/sqlite3/schema.py
@@ -4,8 +4,35 @@ class schema(object):
     def __init__(self, *args, **kwargs):
         self.db = kwargs.get('db')
         self.database_name = kwargs.get('database_name')
+    
+    @property
+    def get_columns(self):
+        columns = ""
         
+        for info in self.table:
+            if info == self.table[-1]:
+                columns += " ".join(info)
+            else:
+                columns += " ".join(info) + ","
+                
+        return columns
+    
+    @property
+    def get_columns_name(self):
+        columns = []
+        
+        for info in self.table:
+            columns.append(info[0])
+            
+        return columns
+    
+    def get_column_info(self, column):
+        for info in self.table:
+            if info[0] == column:
+                return info
+    
     def get_table_description(self):
         cursor = self.db.cursor()
         cursor.execute(sql_table_description % self.database_name)
         data = cursor.fetchall()
+        return data
