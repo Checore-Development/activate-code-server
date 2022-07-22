c760bd9ce54497cc64a2ae3c48bd8f4aa40d5e33
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-22 16:27:22 +0800

    Import Class `Blueprint`

--- activate_code_server/app.py
+++ activate_code_server/app.py
@@ -1,9 +1,24 @@
 from flask import Blueprint
 
-__all__ = ["app"]
 
-app = Blueprint('app', __name__)
+class Application:
+    def __init__(self, *args, **kwargs):
+        self.server = kwargs.get('server')
+        self.database = self.server.database
+        self.app = None
+        self.setup_app()
+        self.setup_urls()
+    
+    def setup_app(self):  
+        self.app = Blueprint('Application', __name__)
+    
+    def setup_urls(self):
+        urls = [
+            ['/', self.get_code, ['GET']]
+        ]
+        
+        for url in urls:
+            self.app.add_url_rule(rule=url[0], view_func=url[1], methods=url[2])
 
-@app.route('/')
-def root():
-    return 'Hello World!'
+    def get_code(self):
+        return self.server.host
