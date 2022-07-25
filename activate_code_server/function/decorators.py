3417cfb5e76fa04e87b3c75bf85641a71b12e55d
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-26 04:03:15 +0800

    Create decorators.py

---
+++ activate_code_server/function/decorators.py
@@ -0,0 +1,18 @@
+from flask import request
+
+def verify_request(func):
+    def wrapper(*args, **kwargs):
+        server = args[0].server
+        
+        if server.authorization:
+            authorization = request.headers.get('Authorization')
+            
+            if authorization is None:
+                return server.status.HTTP_401_UNAUTHORIZED
+            elif authorization != server.authorization:
+                return server.status.HTTP_403_FORBIDDEN
+            else:
+                pass
+                    
+        return func(*args, **kwargs)
+    return wrapper
