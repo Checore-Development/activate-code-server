3ebed7a90b87c1f21655ac5982d401801fa1f34c
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-26 23:37:48 +0800

    Complete `generate_code`

--- activate_code_server/function/function.py
+++ activate_code_server/function/function.py
@@ -9,12 +9,37 @@ class function(object):
         self.encrypted = self.server.encrypted
         
     def generate_code(self, *args, **kwargs):
-        seed = kwargs.get('seed')
-        limit = kwargs.get('limit')
-        expired_date = kwargs.get('expired_date')
+        data = kwargs.get('data')
+        seed = data.get('seed')
+        limit = data.get('limit', 1)
+        expired_date = data.get('expired_date', 2592000)
         
-        if judgment_integer(seed) or seed is None:
-            token = self.generate.token(seed=seed)
-            return token
-        else:
+        if not judgment_integer(seed) or not judgment_integer(limit) or not judgment_integer(expired_date):
             return self.status.HTTP_400_BAD_REQUEST
+        elif limit is not None:
+            if int(limit) < 0:
+                return self.status.HTTP_400_BAD_REQUEST
+        elif expired_date is not None:
+            if int(expired_date) < 0:
+                return self.status.HTTP_400_BAD_REQUEST
+        else:
+            pass
+        
+        _id = self.generate.id
+        token = self.generate.token(seed=seed)
+        
+        if seed is not None and self.database.select(token=token):
+            return self.status.HTTP_400_BAD_REQUEST
+        else:
+            self.database.insert(
+                id=_id, 
+                token=token,  
+                limit=limit,
+                expired_date=expired_date, 
+                encrypted=self.encrypted
+                )
+            
+        return token
+            
+            
+            
