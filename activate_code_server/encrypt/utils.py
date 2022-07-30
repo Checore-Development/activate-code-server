d0fc3955418196dd487a1eed54616dba7d8851e0
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-31 03:58:03 +0800

    Move match_form

--- activate_code_server/encrypt/utils.py
+++ activate_code_server/encrypt/utils.py
@@ -1,15 +1,3 @@
-def match_form(code_format, data):
-    if len(code_format) != len(data):
-        return False
-    else:
-        for code_word, data_word in zip(code_format, data):
-            if code_word == "x":
-                pass
-            else:
-                if code_word != data_word:
-                    return False
-        return True
-    
 def bytes_conversion(value):
     if isinstance(value, bytes):
         return value.decode("utf-32")
--- activate_code_server/function/utils.py
+++ activate_code_server/function/utils.py
@@ -7,3 +7,16 @@ def judgment_integer(value):
             return True
         except:
             return False
+        
+def match_form(code_format, data):
+    if len(code_format) != len(data):
+        return False
+    else:
+        for code_word, data_word in zip(code_format, data):
+            if code_word == "x":
+                pass
+            else:
+                if code_word != data_word:
+                    return False
+        return True
+    
