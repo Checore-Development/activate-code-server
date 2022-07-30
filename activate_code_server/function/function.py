eb769bbcc350b69250154555f8572590fe96ec9e
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-31 02:36:48 +0800

    Return `custum_error` and saving `encrypt_token` to database

--- activate_code_server/function/function.py
+++ activate_code_server/function/function.py
@@ -16,16 +16,16 @@ class function(object):
         seed = data.get('seed')
         limit = data.get('limit', 1)
         expired_date = data.get('expired_date', 2592000)
-        encrypted = data.get('encrypted', 'False')
+        encrypted = data.get('encrypted', self.encrypted)
         
         if not judgment_integer(seed) or not judgment_integer(limit) or not judgment_integer(expired_date):
-            return self.status.HTTP_400_BAD_REQUEST
+            return self.status.HTTP_CUSTOM_400_BAD_REQUEST("Seed, Limit, Expired Date Must Be Integer")
         elif limit is not None:
             if int(limit) < 0:
-                return self.status.HTTP_400_BAD_REQUEST
+                return self.status.HTTP_CUSTOM_400_BAD_REQUEST("Limit Must Be Positive")
         elif expired_date is not None:
             if int(expired_date) < 0:
-                return self.status.HTTP_400_BAD_REQUEST
+                return self.status.HTTP_CUSTOM_400_BAD_REQUEST("Expired Date Must Be Positive")
         else:
             pass
         
@@ -33,15 +33,19 @@ class function(object):
         token = self.generate.token(seed=seed)
         create_date = int(time.time())
         
-        if seed is not None and self.database.select(token=token):
-            return self.status.HTTP_400_BAD_REQUEST
+        encrypt_token = self.encrypt.encode(token, encrypted=encrypted)
+        
+        if encrypt_token is None:
+            return self.status.HTTP_CUSTOM_400_BAD_REQUEST("Unknown Encrypted Method")
+        elif seed is not None and self.database.select(token=encrypt_token, encrypted=encrypted):
+            return self.status.HTTP_CUSTOM_400_BAD_REQUEST("Seed Already Exists")
         else:
             self.database.insert(
                 id=_id, 
-                token=token,
+                token=encrypt_token,
                 limit=limit,
                 expired_date=expired_date, 
-                encrypted=self.encrypted
+                encrypted=encrypted
                 )
             
         return self.status.HTTP_CUSTOM_201_CREATED(
